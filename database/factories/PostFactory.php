05db463d6554f3d41ddf7b725977ef948241a8ae
Chema <chchema26@gmail.com>
2024-10-23 11:27:26 -0600

    Database Setup and lesson: Transaction

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\User;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->unique()->sentence;
+        $slug = Str::slug($title);
+        return [
+            // 'user_id' => fake()->numberBetween(1, User::count()) ,
+            'user_id' => DB::table('users')
+                        ->inRandomOrder()
+                        ->value('id') ,
+            'title' => $title,
+            'slug' => $slug,
+            'excerpt' => fake()->sentence,
+            'description' => fake()->sentence,
+            'is_published' => fake()->boolean,
+            'min_to_read' => fake()->randomDigit()
+        ];
+    }
+}
